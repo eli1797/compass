7442f98a68ad44108d526860fadc5709348a6530
Eli <elibailey97@gmail.com>
2019-07-25 14:00:05 -0400

    Bearing service now operating, connected to home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,7 @@ import { BearingService } from '../bearing.service';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  providers: [BearingService]
 })
 export class HomePage implements OnInit {
 
@@ -17,51 +18,20 @@ export class HomePage implements OnInit {
   trueHeading: any;
 
   constructor(private deviceOrientation: DeviceOrientation, public bearingSerivce: BearingService,  public platform: Platform) {
+    
+  }
+
+  ngOnInit() {
+    this.bearingSerivce.setMyLat(36);
+    this.bearingSerivce.setMyLong(-86);
+
+    // await this.bearingSerivce.subscribe();
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.bearingSerivce.subscribe();
       this.bearingSerivce.logOrientation();
     });
-  }
-
-  ngOnInit() {
-    this.logOrientation();
-    this.bearingSerivce.setMyLat(36);
-    this.bearingSerivce.setMyLong(-86);
-
-    // await this.bearingSerivce.subscribe();
-    console.log('Bearing service magnetic heading');
-    console.log(this.bearingSerivce.getMagneticHeading());
-    console.log(this.bearingSerivce.logOrientation());
-  }
-
-  logOrientation() {
-  // Get the device current compass heading
-  this.deviceOrientation.getCurrentHeading().then((heading) => {
-    this.magneticHeading = heading.magneticHeading;
-    this.trueHeading = heading.trueHeading;
-    console.log(this.magneticHeading, this.trueHeading);
-  }, (err) => {
-    console.log(err);
-  });
-  }
-
-  subscribe() {
-  // Watch the device compass heading change
-  this.subscription = this.deviceOrientation.watchHeading().subscribe((heading) => {
-      this.magneticHeading = heading.magneticHeading;
-      this.trueHeading = heading.trueHeading;
-    }, (err) => {
-      console.log(err);
-    });
-  }
-
-  unsubscribe() {
-  // Stop watching heading change
-  this.subscription.unsubscribe();
-  this.magneticHeading = '';
-  this.trueHeading = '';
   }
 
   direct() {
